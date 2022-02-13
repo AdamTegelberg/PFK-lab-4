08087921674b6d91688d9b9e2cc737567e8d47fe
AdamTegelberg <90842046+AdamTegelberg@users.noreply.github.com>
2022-02-14 00:11:13 +0100

    Add files via upload

---
+++ FifoQueue.java
@@ -0,0 +1,110 @@
+package queue_singlelinkedlist;
+import java.util.*;
+
+public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
+	private QueueNode<E> last;
+	private int size;
+
+	public FifoQueue() {
+		super();
+		last = null;
+		size = 0;
+	}
+
+	/**	
+	 * Inserts the specified element into this queue, if possible
+	 * post:	The specified element is added to the rear of this queue
+	 * @param	e the element to insert
+	 * @return	true if it was possible to add the element 
+	 * 			to this queue, else false
+	 */
+	public boolean offer(E e) {
+		QueueNode<E> node = new QueueNode<E>(e);
+		
+		if (last == null) {
+			last = node;
+			last.next = node;
+			size ++;
+		}
+		else {
+			node.next = last.next;
+			last.next = node;
+			last = node;
+			size ++;
+		}
+		return true;
+	}
+	
+	/**	
+	 * Returns the number of elements in this queue
+	 * @return the number of elements in this queue
+	 */
+	public int size() {		
+		return size;
+	}
+	
+	/**	
+	 * Retrieves, but does not remove, the head of this queue, 
+	 * returning null if this queue is empty
+	 * @return 	the head element of this queue, or null 
+	 * 			if this queue is empty
+	 */
+	public E peek() {
+		if (last == null) {
+			return null;
+		}
+		else {
+			return last.next.element;
+		}
+		
+	}
+
+	/**	
+	 * Retrieves and removes the head of this queue, 
+	 * or null if this queue is empty.
+	 * post:	the head of the queue is removed if it was not empty
+	 * @return 	the head of this queue, or null if the queue is empty 
+	 */
+	public E poll() {
+		E temp = null;
+		QueueNode<E> tempNode;
+		if (last == null) {
+			temp = null;
+		}
+		else if (last.next == last) {
+			temp = last.element;
+			last.next = null;
+			last = null;
+			size --;
+		}
+		else {
+			tempNode = last.next;
+			last.next = null;
+			last.next = tempNode.next;
+			temp = tempNode.element;
+			size --;
+			
+		}
+		return temp;
+	}
+	
+	
+	/**	
+	 * Returns an iterator over the elements in this queue
+	 * @return an iterator over the elements in this queue
+	 */	
+	public Iterator<E> iterator() {
+		return null;
+	}
+	
+	private static class QueueNode<E> {
+		E element;
+		QueueNode<E> next;
+
+		private QueueNode(E x) {
+			element = x;
+			next = null;
+		}
+	}
+
+}
